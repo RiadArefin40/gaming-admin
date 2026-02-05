b7490c519f5bd00b803b121b190a5819ef88d872
git-likhon <arefinriad40@gmail.com>
2026-02-05 17:57:56 +0600

    ok

--- layouts/default.vue
+++ layouts/default.vue
@@ -47,6 +47,9 @@
     <NuxtLink to="/users">
       <v-list-item prepend-icon="mdi-account-group" title="Users" class="menu-item" />
     </NuxtLink>
+               <NuxtLink to="/payment-sms">
+      <v-list-item prepend-icon="mdi-text" title="Referral setting" class="menu-item" />
+    </NuxtLink>
             <NuxtLink to="/commision">
       <v-list-item prepend-icon="mdi-text" title="Referral setting" class="menu-item" />
     </NuxtLink>
